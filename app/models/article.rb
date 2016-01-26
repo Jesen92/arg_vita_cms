201a93e5e507b6ea76ff4bfa22fad17489645693
Jesen92 <hjesenovic@gmail.com>
2016-01-26 19:31:12 +0100

    Napravljen novi filter za kategorije(u grafickom prikazu, čitaj Filterrific), posto je promijenjeno da artikl moze imati vise kategorija morao se u modelu promjeniti :scope za kategorije

--- app/models/article.rb
+++ app/models/article.rb
@@ -119,8 +119,21 @@ class Article < ActiveRecord::Base
                   }
 
   scope :with_category_id, lambda { |category_ids|
-                          where(category_id: [*category_ids])
-                        }
+                          #where(category_id: [*category_ids])
+
+                          # get a reference to the join table
+                          art_cat = ArticleCategory.arel_table
+                          # get a reference to the filtered table
+                          articles = Article.arel_table
+                          # let AREL generate a complex SQL query
+                          where(
+                                ArticleCategory \
+                              .where(art_cat[:article_id].eq(articles[:id])) \
+                              .where(art_cat[:category_id].in([*category_ids].map(&:to_i))) \
+                              .exists
+                            )
+                          }
+
 
   scope :with_material_id, lambda { |material_ids|
                            where(material_id: [*material_ids])
