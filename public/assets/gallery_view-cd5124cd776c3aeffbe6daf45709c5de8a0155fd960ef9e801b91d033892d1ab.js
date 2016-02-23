2764d3d78c6f988277c0bed965d22b1ab4ed1416
Jesen92 <hjesenovic@gmail.com>
2016-02-23 14:36:21 +0100

    Izmijenjen js za gallery view artikla, precompile assets za heroku

--- public/assets/gallery_view-cd5124cd776c3aeffbe6daf45709c5de8a0155fd960ef9e801b91d033892d1ab.js
+++
@@ -1,1 +0,0 @@
-$(document).ready(function(){$(".js--msr-icon").click(function(){var t=$(".js--msr-gall");t.hasClass("col-xs-6 col-md-2")?(t.removeClass("col-xs-6 col-md-2"),t.addClass("col-xs-6 col-md-3")):(t.removeClass("col-xs-6 col-md-3"),t.addClass("col-xs-6 col-md-2"))})});
